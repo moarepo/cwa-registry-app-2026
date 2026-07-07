4ee43e1805eea9cf7611792c4770ee381252876e
ChromeDev1 <armani.brown@moa.gov.jm>
2026-07-07 18:02:57 -0500

    more changes

--- src/pages/Dashborad.vue
+++ src/pages/Dashborad.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-    import { onMounted, computed } from "vue"
+    import { onMounted, computed, ref } from "vue"
     import { useThemeComposable } from '../Composables/useComposables'
     import { motion, AnimatePresence } from 'motion-v';
     import { dash_animation,staggered_animation } from "../animations_config/anime_def"
@@ -8,9 +8,16 @@
     const _store_ = useStore()
     onMounted(async()=>{  await _store_.fetch_registion_count() })
 
+    let delegate_info = computed(()=>{ return _store_.get_Delegates })
+    let exhibitor_info = computed(()=>{ return _store_.get_Exhibitor })
+
     let delegate_amout = computed<number|null>(()=>{ return _store_.get_delegate_count})
     let exhibitor_amout = computed<number|null>(()=>{ return _store_.get_exhibitors_count})
     let media_amout = computed<number|null>(()=>{ return _store_.get_media_count })
+
+    let d_table_head = ["Title","First Name","Middle Initial","Last Name","Gender","Nationality","country_of_residence"]
+
+    function get_data(option:string){ _store_.fetch_from_data(option) }
 
 </script>
 
@@ -35,30 +42,37 @@
                   :class="useThemeComposable() ? 'bg-innerDark border-teal-950'
                   :'bg-off_white border-teal-100'"
                 >
-                   
-                    <motion.div
-                     :initial="staggered_animation(0.2,2,0,-10).initial"
-                     :animate="staggered_animation(0.6,3,0,-10).animate"
-                     :exit="staggered_animation(0.6,2,0,-10).exit"
-                     class="flex justify-center items-center p-2 rounded-2xl text-center space-x-4 border cursor-pointer"
-                     :class="useThemeComposable() ? 'bg-Dark border-teal-950'
-                     :'bg-white border-teal-300'"
+                    <button
+                     @click="get_data('delegates')"
                     >
-                        <h2>Total # Delegate : </h2> 
-                        <h2 class="text-5xl font-light">{{ delegate_amout }}</h2>
-                    </motion.div>
+                        <motion.div
+                         :initial="staggered_animation(0.2,2,0,-10).initial"
+                         :animate="staggered_animation(0.6,3,0,-10).animate"
+                         :exit="staggered_animation(0.6,2,0,-10).exit"
+                         class="flex justify-center items-center p-2 rounded-2xl text-center space-x-4 border cursor-pointer"
+                         :class="useThemeComposable() ? 'bg-Dark border-teal-950'
+                         :'bg-white border-teal-300'"
+                        >
+                            <h2>Total # Delegate : </h2> 
+                            <h2 class="text-5xl font-light">{{ delegate_amout }}</h2>
+                        </motion.div>
+                    </button>
 
-                    <motion.div
-                     :initial="staggered_animation(0.4,2,0,-10).initial"
-                     :animate="staggered_animation(0.8,2,0,-10).animate"
-                     :exit="staggered_animation(0.6,2,0,-10).exit"
-                     class="flex justify-center items-center p-2 rounded-2xl text-center space-x-4 border cursor-pointer"
-                     :class="useThemeComposable() ? 'bg-Dark border-teal-950'
-                     :'bg-white border-teal-300'"
+                    <button
+                     @click="get_data('exhibitor')"
                     >
-                        <h2>Total # Exhibitor : </h2> 
-                        <h2 class="text-5xl font-light">{{ exhibitor_amout }}</h2>
-                    </motion.div>
+                        <motion.div
+                        :initial="staggered_animation(0.4,2,0,-10).initial"
+                        :animate="staggered_animation(0.8,2,0,-10).animate"
+                        :exit="staggered_animation(0.6,2,0,-10).exit"
+                        class="flex justify-center items-center p-2 rounded-2xl text-center space-x-4 border cursor-pointer"
+                        :class="useThemeComposable() ? 'bg-Dark border-teal-950'
+                        :'bg-white border-teal-300'"
+                        >
+                            <h2>Total # Exhibitor : </h2> 
+                            <h2 class="text-5xl font-light">{{ exhibitor_amout }}</h2>
+                        </motion.div>
+                    </button>
 
                     <motion.div
                      :initial="staggered_animation(0.2,2,0,-10).initial"
@@ -75,14 +89,59 @@
                 </motion.div>
 
                 <motion.div
-                 class="flex w-full p-2 rounded-2xl border h-full"
+                 class="flex flex-col space-y-4 w-full p-2 rounded-2xl border h-full"
                  :class="useThemeComposable() ? 'bg-innerDark border-teal-950'
                  :'bg-off_white border-teal-100'"
                 >
 
+                    <div class="w-full p-2 flex">
+
+                        <table
+                         class="table-auto border-separate lg:border-spacing-2 border-spacing-1 border-2 rounded-lg w-full"
+                         :class="useThemeComposable() ? 'bg-innerDark border-teal-950'
+                         : 'bg-white border-gray-100'"
+                        >
+                            <thead>
+                                <tr>
+                                    <th 
+                                    v-for="header in d_table_head"
+                                    :key="header"
+                                    class="border rounded-md p-0.5 text-sm font-semibold"
+                                    :class="useThemeComposable() ? 'bg-Dark border-gray-700'
+                                    : 'border-light_text_colour bg-white'"
+                                    >
+                                    {{ header }}
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr
+                                    v-for="(item,index) in delegate_info"
+                                    :key="index"
+                                    :class="useThemeComposable() ? {
+                                    'bg-Dark': index%2 === 0,
+                                    'bg-teal-950': index%2 === 1
+                                    }:{
+                                    'bg-primary': index%2 === 0,
+                                    'bg-gray-50': index%2 === 1
+                                    }"
+                                >
+                                    <td class="text-center rounded-md px-2 py-2">{{ item.title }}</td>
+                                    <td class="text-center rounded-md px-2 py-2">{{ item.first_name }}</td>
+                                    <td class="text-center rounded-md px-2 py-2">{{ item.middle_initial }}</td>
+                                    <td class="text-center rounded-md px-2 py-2">{{ item.last_name }}</td>
+                                    <td class="text-center rounded-md px-2 py-2">{{ item.gender }}</td>
+                                    <td class="text-center rounded-md px-2 py-2">{{ item.nationality }}</td>
+                                    <td class="text-center rounded-md px-2 py-2">{{ item.country_of_residence }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                    </div>
+                   
+                    {{ exhibitor_info }}
                 </motion.div>
 
-                
             </motion.div>
 
             
--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -1,17 +1,25 @@
 import {defineStore} from "pinia"
 import {supabase} from "../supabase_config/supabaseConfig"
 import {useDisplayStore} from "../store/useDisplayStore"
+import type {Database} from '../utils/database.types'
 
 export const useStore = defineStore('useStore',{
     state:()=>({
+
         delegate_count: 0 as number | null,
         exhibitors_count: 0 as number | null,
-        media_count: 0 as number | null
+        media_count: 0 as number | null,
+        Total_country_count: 0 as number | null,
+
+        Delegates: [] as Database['public']['Tables']['registration_table']['Row'][],
+        Exhibitor: [] as Database['public']['Tables']['exhibitor_table']['Row'][]
     }),
     getters:{
         get_delegate_count: (state) => state.delegate_count,
         get_exhibitors_count: (state) => state.exhibitors_count,
-        get_media_count: (state) => state.media_count
+        get_media_count: (state) => state.media_count,
+        get_Delegates: (state) => state.Delegates,
+        get_Exhibitor: (state) => state.Exhibitor,
     },
     actions:{
         async fetch_registion_count(){
@@ -35,6 +43,52 @@ export const useStore = defineStore('useStore',{
             if(error){ show.change_status(`${error.message}`) }
             if(exhibitors_count.error){ show.change_status(`${exhibitors_count.error.message}`) }
             if(media_count.error){ show.change_status(`${media_count.error.message}`) }
+        },
+
+        async fetch_data_base_on_conutry(country:string){
+
+            const show = useDisplayStore()
+
+            const {count, error} = await supabase
+            .from('registration_table')
+            .select('*',{ count: 'exact', head: true })
+            .ilike('country_of_residence',country)
+
+            if(error){
+                show.change_status(`${error.message}`)
+            }else{
+                this.Total_country_count = count
+            }
+        },
+
+        async fetch_from_data(option:string){
+
+            const show = useDisplayStore()
+
+            switch(option){
+                case "delegates":
+                    const {data,error} = await supabase
+                    .from('registration_table')
+                    .select()
+
+                    if(error){
+                        show.change_status(`${error.message}`)
+                    }else{
+                        this.Delegates = data
+                    }
+                    break;
+                case "exhibitor":
+                    const request = await supabase
+                    .from('exhibitor_table')
+                    .select()
+
+                    if(request.error){
+                        show.change_status(`${request.error.message}`)
+                    }else{
+                        this.Exhibitor = request.data
+                    }
+                    break;  
+            }
         }
     }
 })
